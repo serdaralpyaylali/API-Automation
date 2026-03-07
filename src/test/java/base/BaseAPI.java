f9fef8b849fcbe3f1e5bdc53524a9357cae79775
Alp Yaylalı <serdaralpyaylali@gmail.com>
2026-03-07 21:13:35 +0300

    GitHUb Actions and testng.xml are added

--- src/test/java/base/BaseAPI.java
+++ src/test/java/base/BaseAPI.java
@@ -1,13 +1,22 @@
 package base;
 
-import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
 import config.ConfigReader;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.testng.annotations.BeforeClass;
 
 public class BaseAPI {
 
     @BeforeClass
     public void setup() {
+
         RestAssured.baseURI = ConfigReader.get("base.url");
+
+        RestAssured.filters(
+                new RequestLoggingFilter(LogDetail.ALL),
+                new ResponseLoggingFilter(LogDetail.ALL)
+        );
     }
 }
